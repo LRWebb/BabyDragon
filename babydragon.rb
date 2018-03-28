603ff50d416937061ab4d191e49e373e8e72a1b5
coffeefibercable <LRWebb@users.noreply.github.com>
2018-03-28 17:41:47 -0400

    Creates the BabyDragon class

---
+++ babydragon.rb
@@ -0,0 +1,10 @@
+class BabyDragon
+  def initialize(name)
+    @name = name
+    @asleep = false
+    @stuff_in_belly = 0
+    # 0 is really hungry, 10 is full
+    @stuff_in_intestine = 0
+    # 0 is no need, 10 is need bathroom
+  end
+end
